1d426d08ac271d0fb5593fac9944ef47529e3b5e
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-04 15:23:24 -0700

    pretty print

--- asg3/parse.cpp
+++ asg3/parse.cpp
@@ -26,12 +26,19 @@ vector<vector<string>> Parse::parsingf(){
 	//vector<vector<string>> lines(50);
 	int x = 0;
 	int y = 0;
+
 	while (std::getline(file, str))
 	{
 		std::stringstream ss(str);	// INstantiate stringstream with the current line just read
 		
 		while(ss.good()){
+			if(y == 0){
+			getline(ss,temp,'\t');
+			lines[x][y] = temp;
+			y++;
+			}
 			getline(ss, temp, ','); // Parse token until the first comma and store it in name
+			if(temp.compare("")==0) break;
 			lines[x][y] = temp;
 			y++;
 		}
@@ -41,8 +48,11 @@ vector<vector<string>> Parse::parsingf(){
 		//printf("\n line %s \n", this->lines[x-1][0]);//doesnt work
 		//std::cout<<"cout :"<<str<<"\n" ;//works
 	} 
-	for(int i=0; i<lines.size(); ++i){
-		std::cout << lines[i][0] << "\n";
+	for(int i=0; i<6; ++i){
+		std::cout<<"\n";
+		for(int v = 0; v < 6; ++v)
+		{	if(lines[i][v].compare("")!=0) std::cout <<"arg" <<v<<":"<< lines[i][v] << "\t";
+		}
 		
 	}
 	
@@ -64,7 +74,7 @@ int main(int argc, char** argv)
 	}
 	inFile.close(); // Close file
 	*/
-	
+	cout<<"\n";
 	Parse *pthing = new Parse();
 	pthing->parsingf();
 }
