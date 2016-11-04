e80699df6a152f70db2a6a23d9d9496cf21512b7
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-04 16:45:16 -0700

    basically done with parse

--- asg3/parse.cpp
+++ asg3/parse.cpp
@@ -19,61 +19,61 @@ Parse::~Parse() {
 
 vector<vector<string>> Parse::parsingf(){
 	std::vector<std::vector<std::string>> lines(50,std::vector<string>(50));
-	std::ifstream file("InputTest.txt");
+	std::ifstream file("InputTest2.txt");
 	std::string str;
 	std::string temp;
-	std::string blah;
-	//vector<vector<string>> lines(50);
+
+
 	int x = 0;
 	int y = 0;
 
 	while (std::getline(file, str))
 	{
-		std::stringstream ss(str);	// INstantiate stringstream with the current line just read
+		std::stringstream ss(str);	
 		
 		while(ss.good()){
+			
 			if(y == 0){
-			getline(ss,temp,'\t');
-			lines[x][y] = temp;
-			y++;
+				if(ss.str().find(" ")<ss.str().find("\t")){
+					getline(ss,temp,' ');
+					lines[x][y] = temp;
+					y++;
+				}else{
+					getline(ss,temp,'\t');
+					lines[x][y] = temp;
+					y++;
+				}
+				
 			}
-			getline(ss, temp, ','); // Parse token until the first comma and store it in name
+			getline(ss, temp, ',');
 			if(temp.compare("")==0) break;
 			lines[x][y] = temp;
 			y++;
 		}
 		y = 0;
 		x++;
-		//std::cout<<"cout aray :    "<<this->lines[x-1][0]<<"\n";
-		//printf("\n line %s \n", this->lines[x-1][0]);//doesnt work
-		//std::cout<<"cout :"<<str<<"\n" ;//works
+
 	} 
-	for(int i=0; i<6; ++i){
+	for(int i=0; i<20; ++i){
+		if(lines[i][0].compare("")==0) break;
 		std::cout<<"\n";
-		for(int v = 0; v < 6; ++v)
-		{	if(lines[i][v].compare("")!=0) std::cout <<"arg" <<v<<":"<< lines[i][v] << "\t";
+		for(int v = 0; v < 20; ++v)
+		{	if(lines[i][v].compare("")==0) break;
+			std::cout <<"arg" <<v<<":"<< lines[i][v] << "\t";
 		}
 		
 	}
 	
-
+	cout<<"\n end parse \n";
 	return lines;
 	
 	
 }
 
+
 int main(int argc, char** argv)
 {
-	/*
-	string program [100][100];
-	string line = "";
-	std::ifstream infile("InputTest.txt");
-	while(std::getline(inFile,line)) 
-	{ // Loop through the file lines
-		printf(" line %s \n" , line);
-	}
-	inFile.close(); // Close file
-	*/
+
 	cout<<"\n";
 	Parse *pthing = new Parse();
 	pthing->parsingf();
