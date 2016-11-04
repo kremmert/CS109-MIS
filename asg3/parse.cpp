78e0f4a197ad06b2b1654ccf4a1cf6ac159ef0bc
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-04 14:54:48 -0700

    progress

--- asg3/parse.cpp
+++ asg3/parse.cpp
@@ -4,6 +4,7 @@
 #include<string>
 #include <stdio.h>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 Parse::Parse() {
@@ -20,11 +21,21 @@ vector<vector<string>> Parse::parsingf(){
 	std::vector<std::vector<std::string>> lines(50,std::vector<string>(50));
 	std::ifstream file("InputTest.txt");
 	std::string str;
+	std::string temp;
+	std::string blah;
 	//vector<vector<string>> lines(50);
 	int x = 0;
+	int y = 0;
 	while (std::getline(file, str))
 	{
-		lines[x][0] = str;
+		std::stringstream ss(str);	// INstantiate stringstream with the current line just read
+		
+		while(ss.good()){
+			getline(ss, temp, ','); // Parse token until the first comma and store it in name
+			lines[x][y] = temp;
+			y++;
+		}
+		y = 0;
 		x++;
 		//std::cout<<"cout aray :    "<<this->lines[x-1][0]<<"\n";
 		//printf("\n line %s \n", this->lines[x-1][0]);//doesnt work
