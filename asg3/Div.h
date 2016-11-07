72851e140a90b02e8e6b028b160070ec27fcbd8e
Kyle Remmert <kremmert@users.noreply.github.com>
2016-11-07 13:42:08 -0800

    Update and rename div.h to Div.h

---
+++ asg3/Div.h
@@ -0,0 +1,15 @@
+
+#ifndef DIV_H
+#define DIV_H
+
+class Div {
+public:
+    Div();
+    Div(const Div& orig);
+    virtual ~Div();
+private:
+	void divide(int, int, int );
+
+};
+
+#endif /* DIV_H */
