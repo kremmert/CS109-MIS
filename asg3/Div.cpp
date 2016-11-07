a994557d4382ee7b8f2d84a16baa4f99f74a6cf2
Kyle Remmert <kremmert@users.noreply.github.com>
2016-11-07 13:41:22 -0800

    Update and rename div.cpp to Div.cpp

--- asg3/Div.cpp
+++ asg3/Div.cpp
@@ -1,20 +1,20 @@
 #include "Div.h"
 #include "common.h"
 
-div::div() {
+Div::Div() {
 }
 
-div::div(const div& orig) {
+Div::Div(const Div& orig) {
 }
 
-div::~div() {
+Div::~Div() {
 }
 
 
 //Divide the second parameter by the third parameter and store the result in the first parameter.	 
 //Note: divide	by	zero	should	be	detected and reported.	
 
-void div:: divide(int c, int a, int b){
+void Div:: divide(int c, int a, int b){
     if(b == 0){
         std::cout<<"divide by zero error";
         return;
