18efc0bab284e20ec9e6581181e1cad0396975f0
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-08 17:30:16 -0800

    FINALLY SOLVE THE PROBLEM

--- asg3/Sub.cpp
+++ asg3/Sub.cpp
@@ -21,12 +21,11 @@ void Sub::functor(std::vector<std::string> lines,std::map <std::string,Instructi
 
     std::string a = lines[2];
 	std::string b = lines[3];
-	std::cout << "THIS IS A NULL PTR PROBABLY: "<<lines[3];
+	std::cout << "THIS IS A NULL PTR PROBABLY: "<<mapy[lines[3]];
 	std::cout << "\n is this a num: (a) = "<<isnum(a);
 	std::cout << "\n\nis this a num: (b) ="<<isnum(b);
 	std::cout << "\n\nlines[3]**********"<<mapy[lines[1]];
 	int x,y;
-	std::cout<<"\n lagnlhglag";
 	
 	for (std::map<std::string,Instructions*>::iterator it=mapy.begin(); it!=mapy.end(); ++it)
 		std::cout <<"\n"<< it->first << " => " << it->second << '\n';
@@ -46,8 +45,14 @@ void Sub::functor(std::vector<std::string> lines,std::map <std::string,Instructi
 	}*/
 
 	try{
+		std::cout << "\n lines[3]****444*****"<<mapy[lines[1]]<<lines[1];
+		std::cout << "\n lines[3]****4*****"<<mapy[lines[3]]<<lines[3]<<"   "<<lines[3].substr(0,lines[3].size()-1);
+		if(lines[1].compare(lines[3].substr(0,lines[3].size()-1))==0){
+			std::cout<<"\n they are the same!";
+		}
 		
-		std::cout << "\n\nlines[3]****4*****"<<mapy[lines[3]];
+		std::cout << "\n lines[3]****444*****"<<mapy[lines[1]]<<lines[1];
+		std::cout << "\n lines[3]****4*****"<<mapy[lines[3]]<<lines[3];
 		y = stoi(b);
 		
 		std::cout << "\n\nlines[3]*****5****"<<mapy[lines[3]];
@@ -56,11 +61,8 @@ void Sub::functor(std::vector<std::string> lines,std::map <std::string,Instructi
 	catch(...)
 	{
 		std::cout << "\n\nlines[3]**********"<<mapy[lines[3]];
-		std::cout<<"\n";
-		std::cout<<"\n";
-		std::cout<<"\n";
-		std::cout << "value of lines[3]" << mapy[lines[3]]->getV();
-		y = stoi(mapy[lines[3]]->getV());
+		std::string mn = lines[3].substr(0,lines[3].size()-1);
+		y = std::stoi((mapy[mn])->getV());
 	}
 /*	if(isnum(b)){
 		y = stoi(b);
@@ -68,10 +70,10 @@ void Sub::functor(std::vector<std::string> lines,std::map <std::string,Instructi
 		y = stoi(mapy[lines[3]]->getV());
 	}*/
 	int j = x-y;
-	std::cout<<"\n fuck this";
+	std::cout << "\n\nlines[123]**********"<<mapy[lines[1]];
 	mapy[lines[1]]->setVal(std::to_string(j));
 	
-	std::cout << "*************************************\n\n";
 	std::cout << "\n" <<mapy[lines[1]]->getV() << "\n";
+	std::cout << "\n\nlines[45]**********"<<mapy[lines[1]];
 }
 
