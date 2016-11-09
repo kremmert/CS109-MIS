ed31d3123e1f75528d899ee8d666ed224d8243b8
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-09 10:40:29 -0800

    comments on sub

--- asg3/Sub.cpp
+++ asg3/Sub.cpp
@@ -8,55 +8,47 @@ Sub::Sub(const Sub& orig) {
 
 Sub::~Sub() {
 }
+
+
 //Subtract the third parameter from the second parameter and store the result in the first parameter
-void Sub::subtract(std::vector<std::string> lines){
-    //int * b = a->getVal();
-	//*b = par1 - par2;
-	
-
-}
-
 
 void Sub::functor(std::vector<std::string> lines,std::map <std::string,Instructions *> & mapy){
 
-    std::string a = lines[2];
-	std::string b = lines[3];
+    std::string a = lines[2];// arg 1,constant or var
+	std::string b = lines[3];//arg 2, constant or var
 	int x = 0;
 	int y = 0;
+	//try to convert to int
 	try{
+		//to see if constant or var
 		x = stoi(a);
+		//this is a constant
 	}
 	catch(...)
-	{
+	{	//this is var
 		x = stoi(mapy[lines[2]]->getV());
 	}
 
 
 	try{
-		
+		//this is constant
 		y = stoi(b);
 		
 	}
 	catch(...)
 	{
 		std::string mn = lines[3];
-		//std::string mn = lines[3].substr(0,lines[3].size()-1);
 		if(mapy.find(mn)== mapy.end()){
+			//edge case of nul char at end
 			mn = mn.substr(0,mn.size()-1);
 		}
-/*		if(mn[mn.size()]=='\0')
-		{
-			std::cout <<"\n\nsubstring getting fixed";
-			mn = lines[3].substr(0,lines[3].size()-1);
-			//mn = lines[3].c_str();
-		}*/
+		//this is a var
 		y = std::stoi((mapy[mn])->getV());
 	}
-	
+	//the actual subtraction
 	int j = x-y;
+	//seting the value
 	mapy[lines[1]]->setVal(std::to_string(j));
-	std::cout<<"\n result: "<<mapy[lines[1]]->getV();
-	std::cout<<"\n";
 
 }
 
--- asg3/Sub.h
+++ asg3/Sub.h
@@ -6,13 +6,16 @@
 #include <map>
 class Sub: public Math {
 public:
+	//sub constructor
     Sub();
+	//sub copy constructor
     Sub(const Sub& orig);
+	//sub destructor
     virtual ~Sub();
+	//functor: does the subtraction operation get args from lines and vars from mapy
 	void functor(std::vector<std::string> lines,std::map <std::string,Instructions *> & mapy);
 
 private:
-	void subtract(std::vector<std::string> lines);
 
 };
 
