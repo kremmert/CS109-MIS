c89cf53b9d72699495232f59bfc1d929e3ec92f9
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-09 11:49:42 -0800

    out works to out file

--- asg3/Out.cpp
+++ asg3/Out.cpp
@@ -1,4 +1,6 @@
 #include "Out.h"
+#include <iostream>
+#include <fstream>
 
 
 Out::Out() {
@@ -18,7 +20,9 @@ Instructions * Out::clone(std::vector<std::string> lines){
 
 void Out::functor(std::vector<std::string> lines,std::map <std::string,Instructions *> & mapy){
 
-
+	std::ofstream outputFile;;
+	outputFile.open("Output.out");
+	
 	std::string mn;
 	for(int x = 1; x <lines.size()-1;x++){
 		if(lines[x].compare("")==0) break;
@@ -27,13 +31,15 @@ void Out::functor(std::vector<std::string> lines,std::map <std::string,Instructi
 		if(mapy.find(mn)== mapy.end()){
 			mn = mn.substr(0,mn.size()-1);
 			if(mapy.find(mn)==mapy.end()){
-				std::cout<<"\nArg: "<<lines[x];
+				outputFile<<"Arg: "<<lines[x]<<std::endl;
 				continue;
 			}
 		}
 
-		std::cout<<"\nArg: "<<mn<<"    Val::"<<mapy[mn]->getV();
+		outputFile<<"Arg: "<<mn<<"    Val::"<<mapy[mn]->getV()<<std::endl;
 	}
+	
+	outputFile.close();
 	
 	
 }
