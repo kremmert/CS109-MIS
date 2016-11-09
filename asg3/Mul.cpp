2f299abede489c9cad7550bb7d670b479a404f14
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-08 20:38:29 -0800

    add and mul

--- asg3/Mul.cpp
+++ asg3/Mul.cpp
@@ -29,5 +29,47 @@ T multiply(T res, num... args)
 }
 
 void Mul::functor(std::vector<std::string> lines,std::map <std::string,Instructions *> & mapy){
-	std::cout<<"           ";
+	std::string a = lines[2];
+
+	int x = 0;
+	int y = 1;
+	int v = 2;
+	while(v < lines.size())	{
+
+		try{ 
+			x = stoi(lines[v]);
+		}
+		catch(...) {
+			x = stoi(mapy[lines[v]]->getV());
+		}
+
+		y*=x;
+		if(lines[v+2].compare("")==0)
+		{
+			break;
+		}
+
+		v++;
+	}
+	x = 0;
+
+	try{
+		
+		x = stoi(lines[v+1]);
+		
+	}
+	catch(...)
+	{
+		
+		std::string mn = lines[v+1];
+
+		if(mapy.find(mn)== mapy.end()){
+			mn = mn.substr(0,mn.size()-1);
+		}
+	x = std::stoi((mapy[mn])->getV());
+	}
+	y *= x;
+	
+	mapy[lines[1]]->setVal(std::to_string(y));
+	cout<<"\n value of mul:"<<mapy[lines[1]]->getV();
 }
