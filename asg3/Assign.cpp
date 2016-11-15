a87e7d3bbc6ec9c1cf56c15380489e36d16ad5f8
Kyle Remmert <kremmert@ucsc.edu>
2016-11-15 10:54:11 -0800

    fixed assign

--- asg3/Assign.cpp
+++ asg3/Assign.cpp
@@ -17,22 +17,38 @@ Instructions * Assign::clone(std::vector<std::string> lines){
 
 
 void Assign::functor(std::vector<std::string> lines,std::map <std::string,Instructions *> & mapy){
-	std::string a = lines[1];
+//	std::string a = lines[1];
 	std::string b = lines[2];
 
-	int x = 0;
+//	int x = 0;
 	int y = 0;
 
 		
-	std::string mn = lines[2];
+/*	std::string mn = lines[2];
 	if(mapy.find(mn)== mapy.end()){
 		mn = mn.substr(0,mn.size()-1);
 	}
 	x = stoi(mapy[mn]->getV());
+*/
 
-	mapy[lines[1]]->setVal(std::to_string(x));
+	try{
+		
+		y = stod(b);
+		
+	}
+	catch(...)
+	{
+		std::string mn = lines[2];
+		//a check if the last element has a null character which will
+		//make the string not equal to the one stored in the map
+		if(mapy.find(mn)== mapy.end()){
+			mn = mn.substr(0,mn.size()-1);
+		}
 
-	
+		y = std::stod((mapy[mn])->getV());
+	}
+
+	mapy[lines[1]]->setVal(std::to_string(y));
 	
 	
 }
