5454e80e0cba21b3c6ec126070f3199140666f38
Kyle Remmert <kremmert@ucsc.edu>
2016-11-16 14:32:03 -0800

    fix

--- asg3/TCPServer.cpp
+++ asg3/TCPServer.cpp
@@ -19,27 +19,28 @@ exit(1);
 
 
 int main (int argc,char ** argv){
+
 int sock; // Socket Handler
 struct sockaddr_in serverAddr; // Server Address Structure
  struct sockaddr_in clientAddr; // Client Address Structure
 socklen_t sin_size = sizeof(struct sockaddr_in); // Address Structur Size
 // Try to allocate a socket and if fail we terminate with an error.
-if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) terminate_with_error("Error Creating
-Socket",sock);
+if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    terminate_with_error("Error Creating Socket",sock);
 int sock_opt = 1; // An integer to be used with sock opt
 // Setting socket reuse option so after the program terminates we can acquire the socket again on
-the same port without going into a grace period.
+//the same port without going into a grace period.
 setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(void *)&sock_opt,sizeof (sock_opt));
  serverAddr.sin_family = AF_INET; // Set the communication family
  serverAddr.sin_port = htons(9999); // Set the port number
  serverAddr.sin_addr.s_addr = INADDR_ANY;// Set the socket to operate on all IP address and
-network interfaces available
+//network interfaces available
  bzero(&(serverAddr.sin_zero), 8); // We zero out the rest of the sockaddr_in to make it identical to
-sockaddr
+//sockaddr
 
 
 if (bind(sock, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr)) == -1)
-terminate_with_error("Error Binding",sock);
+    terminate_with_error("Error Binding",sock);
 // Create the socket liten queue with size 10.
 if (listen(sock, 10) == -1) terminate_with_error("Error Listening: ",sock);
 int newsock = accept(sock, (struct sockaddr *)&clientAddr,&sin_size); // Wait for connection
@@ -50,7 +51,7 @@ char buffer[maxBytes]; // alocat buffer of 1 K
 memset(buffer,0,maxBytes); // initialize it.
 int bytes_read = recv (newsock,buffer,maxBytes,0); // read data from the socket
 // if returned number of bytes is bigger that zero then print information about client and the
-message
+
 if ( bytes_read > 0 ) printf ("Received Message from %s:%d\n%s\n",(char
 *)inet_ntoa(clientAddr.sin_addr),clientAddr.sin_port,buffer);
 else perror("Error Receiving Message:"); // else print error through perror.
