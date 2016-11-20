f1c2ca4477eeb0443119425a49b3a718e3e55f5e
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-19 16:49:53 -0800

    praotj

--- asg3/Client.cpp
+++ asg3/Client.cpp
@@ -28,20 +28,22 @@ int main()
 {
 	
 
-	
+	std::cout<<"here1"<<std::endl;
 	Parse p;//parse obj
     std::vector<std::vector<std::string>> lines = p.parsingf("InputTest2.mis");//parsed lines of code
-	if(lines[0][0].compare("")==0) return 0;//if lines is empty, do nothing
+	//if(lines[0][0].compare("")==0) return 0;//if lines is empty, do nothing
 	//std::map <std::string,int> mapy = p.labelget(lines);// get map of labels with line num
     //this->morethanfetch();//start executing code
+	std::cout<<"here3"<<std::endl;
 	int gg=0;
 	for(gg = 0; gg < 50; gg++){
 		if(lines[gg][0].compare("")==0){
-			return gg;
+			break;
 		}
 	}
 
      TCPSocket * test = new TCPSocket((char*)("128.114.104.57"),9999);
+	std::cout<<"here7"<<std::endl;
 	 
 	 
 	test->writeToSocket((char*)(gg),512);
@@ -50,5 +52,6 @@ int main()
 		test->writeToSocket(lines[x][0].c_str(),512);
 		
 	 }
+	std::cout<<"here99"<<std::endl;
      return 0;   
 }
