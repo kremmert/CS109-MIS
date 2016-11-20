a80f4c8989893da6e9dd2332f346c7ce37374446
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-19 18:00:42 -0800

    my code is literally black magic

--- asg3/Client.cpp
+++ asg3/Client.cpp
@@ -39,13 +39,12 @@ int main()
 	
 	Client c;
 
-	std::cout<<"here1"<<std::endl;
 	Parse p;//parse obj
     std::vector<std::vector<std::string>> lines = p.parsingf("InputTest2.mis");//parsed lines of code
 	//if(lines[0][0].compare("")==0) return 0;//if lines is empty, do nothing
 	//std::map <std::string,int> mapy = p.labelget(lines);// get map of labels with line num
     //this->morethanfetch();//start executing code
-	std::cout<<"here3"<<std::endl;
+
 	int gg=0;
 	for(gg = 0; gg < 50; gg++){
 		if(lines[gg][0].compare("")==0){
@@ -54,24 +53,24 @@ int main()
 	}
 
      TCPSocket * test = new TCPSocket((char*)("128.114.104.57"),9999);
-	std::cout<<"here7"<<std::endl;
+
+
 	 
 	 
-	test->writeToSocket(std::to_string(gg).c_str(),512);
+	test->writeToSocket(std::to_string(gg).c_str(),256);
      //test->writeToSocket("Hello there\n",65536);
-	 while(1){
+
 
 		 
-		for(int x = 0; x < gg; x++){
-			argnum = c.howmanyargs(lines[x]);
-			test->writeToSocket(std::to_string(argnum).c_str(),512);
-			for(int starts = 0; starts< argnum; starts++){
-				test->writeToSocket(lines[x][starts].c_str(),512);
-				
-			}
-		
+	for(int x = 0; x < gg; x++){
+		argnum = c.howmanyargs(lines[x]);
+		test->writeToSocket(std::to_string(argnum).c_str(),256);
+		for(int starts = 0; starts< argnum; starts++){
+			test->writeToSocket(lines[x][starts].c_str(),256);
+			
 		}
-	 }
-	std::cout<<"here99"<<std::endl;
+	
+	}
+
      return 0;   
 }
