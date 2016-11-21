948d78708cd86b539327100743a4fe395e386ea8
Kyle Remmert <kremmert@ucsc.edu>
2016-11-21 12:59:10 -0800

    thread fixing

--- asg3/Thread.cpp
+++ asg3/Thread.cpp
@@ -71,13 +71,11 @@ void Thread::start()
 void * Thread::run(void * arg)
 {
 	Thread * me = (Thread *) arg; // Cast the arg to Thread * which is the current thread
-        // Push the cleanup static function to the cleanup functions stack to be invoked within pthread_exit
-        // Notice the close curly bracket that substitute for the complementary pthread_cleanup_pop
-        pthread_cleanup_push(cleanup,arg); }  
-//   	pthread_detach (me->pthread); 
 	me->threadMainBody(arg); //Invoke the thread main function body
-        pthread_exit(NULL); // Invoke pthread_exit to terminate and invoke the cleanup functions.
+    me->cleanup(me);
+    pthread_exit(NULL); // Invoke pthread_exit to terminate and invoke the cleanup functions.
 }
+
 
 // Block until thread finishes execution
 void Thread::waitForRunToFinish ()
