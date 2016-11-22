0003ac95ff1a58f6e68deb3a170d9784c14fef26
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-22 15:58:49 -0800

    connection formating

--- asg3/Connection.h
+++ asg3/Connection.h
@@ -4,14 +4,14 @@
 class Connection : public Thread //Inherit from Thread
 {
  private:
- TCPSocket * tcpSocket; // TCP Socket for communication with client
- Connection * next_connection; // A way to build a linked list of connections for the
-garbage collector to be able to track them
+	TCPSocket * tcpSocket; // TCP Socket for communication with client
+	Connection * next_connection; // A way to build a linked list of connections for the
+	garbage collector to be able to track them
  public:
- Connection(TCPSocket * p_tcpSocket); // Constructor: Set client connected TCP socket
- void * threadMainBody (void * arg); // Main thread body that serves the connection
- void setNextConnection(Connection * connection); // Set the next pointer: used by the
-Garbage Collector
- Connection * getNextConnection (); // Get a pointer to the next connection
- ~Connection(); // Destructor
+	Connection(TCPSocket * p_tcpSocket); // Constructor: Set client connected TCP socket
+	void * threadMainBody (void * arg); // Main thread body that serves the connection
+	void setNextConnection(Connection * connection); // Set the next pointer: used by the
+	Garbage Collector
+	Connection * getNextConnection (); // Get a pointer to the next connection
+	~Connection(); // Destructor
 };
