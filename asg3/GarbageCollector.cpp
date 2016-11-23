9e60623f84b85e69b9c3356ab0f4ca0fad4af471
Petar Zaninovich <pzaninov@ucsc.edu>
2016-11-22 16:07:20 -0800

    garbage update

--- asg3/GarbageCollector.cpp
+++ asg3/GarbageCollector.cpp
@@ -12,15 +12,32 @@ void GarbageCollector::addConnection(Connection * connection)
 		tail = connection;
 	 }
 }
-void GarbageCollector::cleanup() {// Clean up all finished threads
- Connection * cur = head; // set cur to head
- for ( ; cur!= NULL && !cur->isRunning(); ) { // Loop and delete all finished connection from the front
-of the list
-cur->waitForRunToFinish(); // Wait for the thread
- Connection * cur1 = cur; // get a pointer to it
- cur = cur->getNextConnection(); // Advance cur
-delete (cur1); // Delete finished thread
- }
- head = cur; // Set head to cur
- if ( head == NULL) tail=NULL; // If list is empty set tail to NULL
+void GarbageCollector::cleanup() 
+{// Clean up all finished threads
+	Connection * cur = head; // set cur to head
+	for ( ; cur!= NULL && !cur->isRunning(); ) 
+	{ // Loop and delete all finished connection from the front of the list
+		cur->waitForRunToFinish(); // Wait for the thread
+		Connection * cur1 = cur; // get a pointer to it
+		cur = cur->getNextConnection(); // Advance cur
+		delete (cur1); // Delete finished thread
+	}
+	head = cur; // Set head to cur
+	if ( head == NULL) tail=NULL; // If list is empty set tail to NULL
 }
+
+// Invoked upon program termination to Gracefully wait for all running threads to finish
+void GarbageCollector::terminate() 
+{
+	Connection * cur = head; //set cur to jead
+	for ( ; cur!= NULL; ) 
+	{ // Loop on all connections
+		cur->waitForRunToFinish(); // wait for the current connection to finish
+		Connection * cur1 = cur; // get a pointer to it
+		cur = cur->getNextConnection(); // advance cur
+		delete (cur1); // delete the current connection
+	}
+	head = tail=NULL; // set head and tail to NULL as the list should be empty
+}
+
+GarbageCollector::~GarbageCollector() { terminate(); // Invoke terminate to dispose all connections}
